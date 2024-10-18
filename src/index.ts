4fc6926fdee8b5dbe19526d8fb702dde03c3d3c0
Stanislav Spatari <spatari.stanislav@gmail.com>
2024-10-18 16:32:56 +0300

    feat: init commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Hello World User Script
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  A simple hello world script
+// @author       You
+// @match        http://*/*, https://*/*
+// @grant        none
+// ==/UserScript==
+
+(() => {
+  console.log("Hello World from a User Script!");
+})();
