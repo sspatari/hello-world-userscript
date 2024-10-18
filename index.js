909acd8365ab2b63dbaacca6bd59d008361f8ab9
sspatari <sspatari@users.noreply.github.com>
2024-10-18 15:17:58 +0000

    deploy: 1f46cb2e5aac20085410118884e965c99bef6bf0

--- index.js
+++ index.js
@@ -7,8 +7,8 @@
 // @author       You
 // @match        https://github.com/*
 // @grant        none
-// @updateURL    https://sspatari.github.io/hello-world-userscript/index.js
-// @downloadURL  https://sspatari.github.io/hello-world-userscript/index.js
+// @updateURL    https://raw.githubusercontent.com/sspatari/hello-world-userscript/refs/heads/gh-pages/index.js
+// @downloadURL  https://raw.githubusercontent.com/sspatari/hello-world-userscript/refs/heads/gh-pages/index.js
 // ==/UserScript==
 (function () {
     console.log("Hello World from a User Script!");
