14badf15d21689affe50c2a79f531f8beaf6c8e8
sspatari <sspatari@users.noreply.github.com>
2024-10-18 14:34:24 +0000

    deploy: c7d8e9b8cc0bc83c9171a87255215a55595e3a3f

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 // @author       You
 // @match        http://*/*, https://*/*
 // @grant        none
+// @require      https://sspatari.github.io/hello-world-userscript/index.js
 // ==/UserScript==
 (function () {
     console.log("Hello World from a User Script!");
