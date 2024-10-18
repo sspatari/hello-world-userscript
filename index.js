ee418b633df7948aa767377cef4f67919ee1abe7
sspatari <sspatari@users.noreply.github.com>
2024-10-18 14:55:04 +0000

    deploy: 0c79681e43355f44ce5399575fababd48e779c1e

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 // ==UserScript==
 // @name         Hello World User Script
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  A simple hello world script
 // @author       You
 // @match        https://github.com/*
 // @grant        none
+// @updateURL    https://sspatari.github.io/hello-world-userscript/index.js
+// @downloadURL  https://sspatari.github.io/hello-world-userscript/index.js
 // ==/UserScript==
 (function () {
     console.log("Hello World from a User Script!");
