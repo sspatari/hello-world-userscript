0c79681e43355f44ce5399575fababd48e779c1e
Stanislav Spatari <spatari.stanislav@gmail.com>
2024-10-18 17:54:41 +0300

    chore: auto download and prompt for install

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         Hello World User Script
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  A simple hello world script
 // @author       You
 // @match        https://github.com/*
 // @grant        none
+// @updateURL    https://sspatari.github.io/hello-world-userscript/index.js
+// @downloadURL  https://sspatari.github.io/hello-world-userscript/index.js
 // ==/UserScript==
 
 (() => {
