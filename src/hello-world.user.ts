351f11b79a5e4609d0b7356facda0bc98961d8fd
Stanislav Spatari <spatari.stanislav@gmail.com>
2024-10-21 14:31:10 +0300

    chore: testing the script

--- src/hello-world.user.ts
+++ src/hello-world.user.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hello World User Script
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  A simple hello world script
 // @author       You
 // @match        https://github.com/*
@@ -11,5 +11,5 @@
 // ==/UserScript==
 
 (() => {
-  console.log("Best Hello World from a User Script!");
+  console.log("Testing Best Hello World from a User Script!");
 })();
